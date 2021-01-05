a1fee3f45f96a48e7396484184da82a60b65187a
Manasi Wader <52437677+WaderManasi@users.noreply.github.com>
2021-01-05 09:55:58 +0530

    js file added

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
